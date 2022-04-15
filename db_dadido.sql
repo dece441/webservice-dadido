a8611508584f05d426ddb91d74debe4c8cfa7ed2
dece441 <danieldc01@gmail.com>
2022-04-15 18:30:37 +0700

    adding dummy data in sql

---
+++ db_dadido.sql
@@ -0,0 +1,256 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 15, 2022 at 01:28 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_dadido`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_collection`
+--
+
+CREATE TABLE `tbl_collection` (
+  `id` int(11) NOT NULL,
+  `nama_collection` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `pathfile_server` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_collection`
+--
+
+INSERT INTO `tbl_collection` (`id`, `nama_collection`, `url`, `description`, `pathfile_server`) VALUES
+(1, 'Daniel', 'doremi.com', 'Ini adalah collection saya yang dimana banyak isinya', 'http://test.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_favorit`
+--
+
+CREATE TABLE `tbl_favorit` (
+  `id` int(11) NOT NULL,
+  `id_collection` int(11) NOT NULL,
+  `id_item` int(11) NOT NULL,
+  `id_profile` int(11) NOT NULL,
+  `status_favorit` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_item`
+--
+
+CREATE TABLE `tbl_item` (
+  `id` int(11) NOT NULL,
+  `id_collection` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `nama_file` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `deskripsi` varchar(255) NOT NULL,
+  `status_sell` int(11) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_item`
+--
+
+INSERT INTO `tbl_item` (`id`, `id_collection`, `id_user`, `nama_file`, `url`, `deskripsi`, `status_sell`, `price`) VALUES
+(1, 1, 1, 'Gambar Indah', 'http://tempatnya', 'Dibuat Oleh daniel', 1, 12),
+(2, 1, 1, 'Gambar Daniel', 'http://lokasi', 'Dibuat Oleh Daniel', 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_profile`
+--
+
+CREATE TABLE `tbl_profile` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_collection` int(11) NOT NULL,
+  `id_wallet` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_profile`
+--
+
+INSERT INTO `tbl_profile` (`id`, `id_user`, `id_collection`, `id_wallet`) VALUES
+(1, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_transaction`
+--
+
+CREATE TABLE `tbl_transaction` (
+  `id` int(11) NOT NULL,
+  `id_item` int(11) NOT NULL,
+  `id_profile_beli` int(11) NOT NULL,
+  `id_profile_jual` int(11) NOT NULL,
+  `waktu_transaksi` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `username`, `fullname`, `email`, `password`) VALUES
+(1, 'dece441', 'Daniel Christian', 'daniel@email.com', 'daniel123'),
+(2, 'Budi123', 'Budi Ariyansa', 'Budi@email.com', 'Budi123'),
+(3, 'Ardo1', 'leonardo', 'ardo@gmail.com', 'ardo123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_wallet`
+--
+
+CREATE TABLE `tbl_wallet` (
+  `id` int(11) NOT NULL,
+  `total_fund` float NOT NULL,
+  `wallet_type` varchar(255) NOT NULL,
+  `contract_address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_wallet`
+--
+
+INSERT INTO `tbl_wallet` (`id`, `total_fund`, `wallet_type`, `contract_address`) VALUES
+(1, 10, 'dadido', 'kDSJhfkjsdhflksajhdkajlsdhflkjahsdfjkhsd');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_collection`
+--
+ALTER TABLE `tbl_collection`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_favorit`
+--
+ALTER TABLE `tbl_favorit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_item`
+--
+ALTER TABLE `tbl_item`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_profile`
+--
+ALTER TABLE `tbl_profile`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_transaction`
+--
+ALTER TABLE `tbl_transaction`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_wallet`
+--
+ALTER TABLE `tbl_wallet`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_collection`
+--
+ALTER TABLE `tbl_collection`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_favorit`
+--
+ALTER TABLE `tbl_favorit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_item`
+--
+ALTER TABLE `tbl_item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_profile`
+--
+ALTER TABLE `tbl_profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_transaction`
+--
+ALTER TABLE `tbl_transaction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_wallet`
+--
+ALTER TABLE `tbl_wallet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
