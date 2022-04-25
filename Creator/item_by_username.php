8761e1e7b15f9f7ee39f6b4df33b2151968663d5
BUDIARIYANSA <672019076@student.uksw.edu>
2022-04-25 16:11:58 +0700

    fixing error

--- Creator/item_by_username.php
+++ Creator/item_by_username.php
@@ -5,8 +5,8 @@
     $sql = "SELECT i.id, i.file_name, i.image_url, i.description, i.price
             FROM tbl_item i, tbl_user u, tbl_profile p
             WHERE i.id_collection = p.id_collection
-            AND i.id_profile = p.id
-            AND i.id_profile = u.id
+            AND i.id_profile_creator = p.id
+            AND i.id_profile_creator = u.id
             AND u.username = '$username'";
     $query = mysqli_query($my_conn, $sql);
     if($query){
