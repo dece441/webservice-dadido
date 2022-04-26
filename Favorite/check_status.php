99d633f7066dfe781f6e648afb6f5ef947532ece
dece441 <danieldc01@gmail.com>
2022-04-26 15:13:04 +0700

    insert new fav with status checker

---
+++ Favorite/check_status.php
@@ -0,0 +1,9 @@
+<?php
+    $username_creator = $_POST["username_creator"];
+    $username_seeing = $_POST["username_seeing"];
+    $id_item = $_POST["id_item"];
+    $collection_name = $_POST["collection_name"];
+
+    
+
+?>
